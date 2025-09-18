a7dd691ba58748d23f08d6a8db7b14b9a6dde30d
madhukiranp1227 <madhukiranp27@gmail.com>
2025-09-17 18:48:09 -0700

    finding the path

--- src/App.js
+++ src/App.js
@@ -3,16 +3,26 @@ import ReactDOM from "react-dom/client";
 import "/index.css";
 import { Header } from "./components/Header.js";
 import Body from "./components/Body.js";
+import About from "./components/About.js";
+import Contact from "./components/Contact.js";
+import Error from "./components/Error.js";
 
-const AppLayout = () => {
-    console.log(<Body />);
-    return (
-        <div className="app">
-            <Header />
-            <Body />
-        </div>
-    );
-};
+// v7: pull router primitives from "react-router"
+import { createBrowserRouter, RouterProvider } from "react-router";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout />);
+const AppLayout = () => (
+    <div className="app">
+        <Header />
+        <Body />
+    </div>
+);
+
+const appRouter = createBrowserRouter([
+    { path: "/", element: <AppLayout />, errorElement: <Error /> },
+    { path: "/about", element: <About /> },
+    { path: "/contact", element: <Contact /> },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <RouterProvider router={appRouter} />
+);
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,15 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { LOGO_URL } from "../utils/constants";
 export const Header = () => {
     const [btnNameReact, setbtnNameReact] = useState("Login");
+    console.log("Header render");
+
+    //if no dependency array => useEffect is called on every render of the component
+    // if the dependency array is empty => useEffect is called only on the initial render of the component
+    // if the dependecy array is [btnNameReact] => useEffect is called whenver btnNameReact is updated.
+    useEffect(() => {
+        console.log("useEffect called");
+    }, [btnNameReact]);
     return (
         <div className="header">
             <div className="logo-container">
